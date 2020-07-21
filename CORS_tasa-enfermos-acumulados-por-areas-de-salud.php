2750a7b2128c50186a8cc6f6838906c4de5cacbd
mvb <mvaronabueno@gmail.com>
2020-07-21 04:09:16 +0200

    Added last available date, it admits empty date

--- CORS_tasa-enfermos-acumulados-por-areas-de-salud.php
+++ CORS_tasa-enfermos-acumulados-por-areas-de-salud.php
@@ -14,6 +14,29 @@
 
 		$json = file_get_contents($url);
 		echo $json;
+
+	} else if (!isset($_GET['date']) && isset($_GET['office'])){
+
+		$date = date('m/d/Y');
+
+		do {
+
+			$date = date('m/d/Y', strtotime("$date -1 days"));
+
+			$day = date('d', strtotime($date));
+			$month = date('m', strtotime($date));
+			$year = date('Y', strtotime($date));
+			$office = $_GET['office'];
+
+			$url = "https://data.opendatasoft.com/explore/dataset/tasa-enfermos-acumulados-por-areas-de-salud@jcyl/download/?format=json&disjunctive.zbs_geo=true&refine.fecha=" . $year . "&refine.fecha=" . $year . "%2F" . $month . "&refine.fecha=" . $year . "%2F" . $month . "%2F" . $day . "&refine.nombregerencia=" . $office . "&timezone=Europe/Berlin";
+
+			$json = file_get_contents($url);
+			$jsonArray = json_decode($json);
+
+		} while (count($jsonArray) == 0);
+		
+		echo $json;
+
 	}
 
 ?>
