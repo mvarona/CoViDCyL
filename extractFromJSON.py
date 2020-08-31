7bb8e3b97f16667290c4e41ad5c30ccf03b53059
mvb <mvaronabueno@gmail.com>
2020-08-31 15:38:19 +0200

    Added towns and python file to extract from json

---
+++ extractFromJSON.py
@@ -0,0 +1,77 @@
+import json
+
+def checkIfNameHasArticle(name):
+	if "(" in name:
+		return True
+	return False
+
+def moveArticle(name):
+	mainName = name.split(' (')[0]
+	article = (name.split(' (')[1]).split(')')[0]
+	return article + " " + mainName
+
+def checkIfNameIsACapitalCity(name):
+	capitalCities = ["LEON", "PALENCIA", "BURGOS", "SORIA", "SEGOVIA", "AVILA", "SALAMANCA", "ZAMORA", "VALLADOLID"];
+	if name in capitalCities:
+		return True
+	return False
+
+def saveJSONFromJSON():
+
+	result = {}
+	
+	with open('tasa-enfermos-acumulados-por-areas-de-salud.json') as f:
+		data = json.load(f)
+	
+	for item in data:
+
+		if "/" in item["fields"]["municipio"]:
+
+			municipios = item["fields"]["municipio"].split("/")
+
+			for municipio in municipios:
+
+				if (checkIfNameHasArticle(municipio)):
+					municipio = moveArticle(municipio)
+
+				if (checkIfNameIsACapitalCity(municipio)):
+					municipio = item["fields"]["zbs_geo"]
+
+				result[municipio] = {}
+				result[municipio]["centro"] = item["fields"]["centro"]
+				result[municipio]["gerencia"] = item["fields"]["gerencia"]
+				result[municipio]["cs"] = item["fields"]["cs"]
+
+		else:
+
+			municipio = item["fields"]["municipio"]
+
+			if (checkIfNameHasArticle(municipio)):
+					municipio = moveArticle(municipio)
+
+			if (checkIfNameIsACapitalCity(municipio)):
+					municipio = item["fields"]["zbs_geo"]
+
+			result[municipio] = {}
+			result[municipio]["centro"] = item["fields"]["centro"]
+			result[municipio]["gerencia"] = item["fields"]["gerencia"]
+			result[municipio]["cs"] = item["fields"]["cs"]
+
+	#with open('municipios.json', 'w') as fp:
+	#	json.dump(result, fp)
+
+def printFieldFromJSON():
+	result = []
+	
+	with open('municipios.json') as f:
+		data = json.load(f)
+	
+	for item in data:
+		result.append(item)
+
+	print(result)
+	print(len(result))
+
+#saveJSONFromJSON()
+
+printFieldFromJSON()
